8e73514cb96e0f0bd40706c0f54855ad45239bf7
RuslanKontrobaev <ruslan.kontrobaev@mail.ru>
2023-02-06 17:06:37 +0400

    Upload project

---
+++ Project/ScaleImageApp/ScaleApp_form.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form_main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_main));
+            this.button_load = new System.Windows.Forms.Button();
+            this.textBox_scale = new System.Windows.Forms.TextBox();
+            this.button_scale = new System.Windows.Forms.Button();
+            this.label_scale = new System.Windows.Forms.Label();
+            this.radioButton_bilinear = new System.Windows.Forms.RadioButton();
+            this.groupBox_choice_of_algorithm = new System.Windows.Forms.GroupBox();
+            this.radioButton_bicubic = new System.Windows.Forms.RadioButton();
+            this.groupBox_actions = new System.Windows.Forms.GroupBox();
+            this.button_save_image = new System.Windows.Forms.Button();
+            this.groupBox_settings = new System.Windows.Forms.GroupBox();
+            this.pictureBox_original_image = new System.Windows.Forms.PictureBox();
+            this.pictureBox_scale_image = new System.Windows.Forms.PictureBox();
+            this.groupBox_original_image = new System.Windows.Forms.GroupBox();
+            this.label_resolution_original = new System.Windows.Forms.Label();
+            this.groupBox_scale_image = new System.Windows.Forms.GroupBox();
+            this.label_resolution_scale = new System.Windows.Forms.Label();
+            this.groupBox_choice_of_algorithm.SuspendLayout();
+            this.groupBox_actions.SuspendLayout();
+            this.groupBox_settings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_original_image)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_scale_image)).BeginInit();
+            this.groupBox_original_image.SuspendLayout();
+            this.groupBox_scale_image.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_load
+            // 
+            this.button_load.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_load.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button_load.Location = new System.Drawing.Point(7, 22);
+            this.button_load.Name = "button_load";
+            this.button_load.Size = new System.Drawing.Size(129, 86);
+            this.button_load.TabIndex = 1;
+            this.button_load.Text = "Загрузить изображение";
+            this.button_load.UseVisualStyleBackColor = true;
+            this.button_load.Click += new System.EventHandler(this.button_load_Click);
+            // 
+            // textBox_scale
+            // 
+            this.textBox_scale.Location = new System.Drawing.Point(223, 40);
+            this.textBox_scale.Name = "textBox_scale";
+            this.textBox_scale.Size = new System.Drawing.Size(205, 21);
+            this.textBox_scale.TabIndex = 7;
+            this.textBox_scale.Text = "1";
+            // 
+            // button_scale
+            // 
+            this.button_scale.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_scale.Enabled = false;
+            this.button_scale.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button_scale.Location = new System.Drawing.Point(143, 22);
+            this.button_scale.Name = "button_scale";
+            this.button_scale.Size = new System.Drawing.Size(129, 40);
+            this.button_scale.TabIndex = 3;
+            this.button_scale.Text = "Масштабировать изображение";
+            this.button_scale.UseVisualStyleBackColor = true;
+            this.button_scale.Click += new System.EventHandler(this.button_scale_Click);
+            // 
+            // label_scale
+            // 
+            this.label_scale.AutoSize = true;
+            this.label_scale.Location = new System.Drawing.Point(223, 22);
+            this.label_scale.Name = "label_scale";
+            this.label_scale.Size = new System.Drawing.Size(246, 15);
+            this.label_scale.TabIndex = 5;
+            this.label_scale.Text = "Коэффициент масштабирования (>= 1 и <= 50):";
+            // 
+            // radioButton_bilinear
+            // 
+            this.radioButton_bilinear.AutoSize = true;
+            this.radioButton_bilinear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.radioButton_bilinear.Location = new System.Drawing.Point(7, 22);
+            this.radioButton_bilinear.Name = "radioButton_bilinear";
+            this.radioButton_bilinear.Size = new System.Drawing.Size(158, 19);
+            this.radioButton_bilinear.TabIndex = 5;
+            this.radioButton_bilinear.TabStop = true;
+            this.radioButton_bilinear.Text = "Билинейная интерполяция";
+            this.radioButton_bilinear.UseVisualStyleBackColor = true;
+            // 
+            // groupBox_choice_of_algorithm
+            // 
+            this.groupBox_choice_of_algorithm.Controls.Add(this.radioButton_bicubic);
+            this.groupBox_choice_of_algorithm.Controls.Add(this.radioButton_bilinear);
+            this.groupBox_choice_of_algorithm.Location = new System.Drawing.Point(7, 22);
+            this.groupBox_choice_of_algorithm.Name = "groupBox_choice_of_algorithm";
+            this.groupBox_choice_of_algorithm.Size = new System.Drawing.Size(209, 87);
+            this.groupBox_choice_of_algorithm.TabIndex = 7;
+            this.groupBox_choice_of_algorithm.TabStop = false;
+            this.groupBox_choice_of_algorithm.Text = "Агоритм интерполяции";
+            // 
+            // radioButton_bicubic
+            // 
+            this.radioButton_bicubic.AutoSize = true;
+            this.radioButton_bicubic.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.radioButton_bicubic.Location = new System.Drawing.Point(7, 48);
+            this.radioButton_bicubic.Name = "radioButton_bicubic";
+            this.radioButton_bicubic.Size = new System.Drawing.Size(170, 19);
+            this.radioButton_bicubic.TabIndex = 6;
+            this.radioButton_bicubic.TabStop = true;
+            this.radioButton_bicubic.Text = "Бикубическая интерполяция";
+            this.radioButton_bicubic.UseVisualStyleBackColor = true;
+            // 
+            // groupBox_actions
+            // 
+            this.groupBox_actions.Controls.Add(this.button_save_image);
+            this.groupBox_actions.Controls.Add(this.button_load);
+            this.groupBox_actions.Controls.Add(this.button_scale);
+            this.groupBox_actions.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox_actions.Location = new System.Drawing.Point(12, 380);
+            this.groupBox_actions.Name = "groupBox_actions";
+            this.groupBox_actions.Size = new System.Drawing.Size(284, 115);
+            this.groupBox_actions.TabIndex = 8;
+            this.groupBox_actions.TabStop = false;
+            this.groupBox_actions.Text = "Действия";
+            // 
+            // button_save_image
+            // 
+            this.button_save_image.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_save_image.Enabled = false;
+            this.button_save_image.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button_save_image.Location = new System.Drawing.Point(142, 68);
+            this.button_save_image.Name = "button_save_image";
+            this.button_save_image.Size = new System.Drawing.Size(129, 40);
+            this.button_save_image.TabIndex = 2;
+            this.button_save_image.Text = "Сохранить изображение";
+            this.button_save_image.UseVisualStyleBackColor = true;
+            this.button_save_image.Click += new System.EventHandler(this.button_save_image_Click);
+            // 
+            // groupBox_settings
+            // 
+            this.groupBox_settings.Controls.Add(this.groupBox_choice_of_algorithm);
+            this.groupBox_settings.Controls.Add(this.label_scale);
+            this.groupBox_settings.Controls.Add(this.textBox_scale);
+            this.groupBox_settings.Enabled = false;
+            this.groupBox_settings.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox_settings.Location = new System.Drawing.Point(303, 380);
+            this.groupBox_settings.Name = "groupBox_settings";
+            this.groupBox_settings.Size = new System.Drawing.Size(470, 115);
+            this.groupBox_settings.TabIndex = 9;
+            this.groupBox_settings.TabStop = false;
+            this.groupBox_settings.Text = "Настройка параметров";
+            // 
+            // pictureBox_original_image
+            // 
+            this.pictureBox_original_image.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.pictureBox_original_image.Location = new System.Drawing.Point(6, 41);
+            this.pictureBox_original_image.Name = "pictureBox_original_image";
+            this.pictureBox_original_image.Size = new System.Drawing.Size(370, 315);
+            this.pictureBox_original_image.TabIndex = 0;
+            this.pictureBox_original_image.TabStop = false;
+            // 
+            // pictureBox_scale_image
+            // 
+            this.pictureBox_scale_image.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.pictureBox_scale_image.Location = new System.Drawing.Point(6, 41);
+            this.pictureBox_scale_image.Name = "pictureBox_scale_image";
+            this.pictureBox_scale_image.Size = new System.Drawing.Size(359, 315);
+            this.pictureBox_scale_image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox_scale_image.TabIndex = 0;
+            this.pictureBox_scale_image.TabStop = false;
+            // 
+            // groupBox_original_image
+            // 
+            this.groupBox_original_image.Controls.Add(this.label_resolution_original);
+            this.groupBox_original_image.Controls.Add(this.pictureBox_original_image);
+            this.groupBox_original_image.Location = new System.Drawing.Point(19, 12);
+            this.groupBox_original_image.Name = "groupBox_original_image";
+            this.groupBox_original_image.Size = new System.Drawing.Size(376, 362);
+            this.groupBox_original_image.TabIndex = 11;
+            this.groupBox_original_image.TabStop = false;
+            this.groupBox_original_image.Text = "Оригинальное изображение";
+            // 
+            // label_resolution_original
+            // 
+            this.label_resolution_original.AutoSize = true;
+            this.label_resolution_original.Location = new System.Drawing.Point(7, 21);
+            this.label_resolution_original.Name = "label_resolution_original";
+            this.label_resolution_original.Size = new System.Drawing.Size(70, 15);
+            this.label_resolution_original.TabIndex = 1;
+            this.label_resolution_original.Text = "Разрешение:";
+            this.label_resolution_original.Visible = false;
+            // 
+            // groupBox_scale_image
+            // 
+            this.groupBox_scale_image.Controls.Add(this.label_resolution_scale);
+            this.groupBox_scale_image.Controls.Add(this.pictureBox_scale_image);
+            this.groupBox_scale_image.Location = new System.Drawing.Point(402, 12);
+            this.groupBox_scale_image.Name = "groupBox_scale_image";
+            this.groupBox_scale_image.Size = new System.Drawing.Size(372, 362);
+            this.groupBox_scale_image.TabIndex = 12;
+            this.groupBox_scale_image.TabStop = false;
+            this.groupBox_scale_image.Text = "Отмасштабированное изображение";
+            // 
+            // label_resolution_scale
+            // 
+            this.label_resolution_scale.AutoSize = true;
+            this.label_resolution_scale.Location = new System.Drawing.Point(6, 21);
+            this.label_resolution_scale.Name = "label_resolution_scale";
+            this.label_resolution_scale.Size = new System.Drawing.Size(70, 15);
+            this.label_resolution_scale.TabIndex = 2;
+            this.label_resolution_scale.Text = "Разрешение:";
+            this.label_resolution_scale.Visible = false;
+            // 
+            // Form_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 501);
+            this.Controls.Add(this.groupBox_scale_image);
+            this.Controls.Add(this.groupBox_original_image);
+            this.Controls.Add(this.groupBox_settings);
+            this.Controls.Add(this.groupBox_actions);
+            this.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form_main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторная работа №1. Масштабирование изображения";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_main_FormClosing);
+            this.groupBox_choice_of_algorithm.ResumeLayout(false);
+            this.groupBox_choice_of_algorithm.PerformLayout();
+            this.groupBox_actions.ResumeLayout(false);
+            this.groupBox_settings.ResumeLayout(false);
+            this.groupBox_settings.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_original_image)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_scale_image)).EndInit();
+            this.groupBox_original_image.ResumeLayout(false);
+            this.groupBox_original_image.PerformLayout();
+            this.groupBox_scale_image.ResumeLayout(false);
+            this.groupBox_scale_image.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button_load;
+        private System.Windows.Forms.TextBox textBox_scale;
+        private System.Windows.Forms.Button button_scale;
+        private System.Windows.Forms.Label label_scale;
+        private System.Windows.Forms.RadioButton radioButton_bilinear;
+        private System.Windows.Forms.GroupBox groupBox_choice_of_algorithm;
+        private System.Windows.Forms.RadioButton radioButton_bicubic;
+        private System.Windows.Forms.GroupBox groupBox_actions;
+        private System.Windows.Forms.Button button_save_image;
+        private System.Windows.Forms.GroupBox groupBox_settings;
+        private System.Windows.Forms.PictureBox pictureBox_original_image;
+        private System.Windows.Forms.PictureBox pictureBox_scale_image;
+        private System.Windows.Forms.GroupBox groupBox_original_image;
+        private System.Windows.Forms.GroupBox groupBox_scale_image;
+        private System.Windows.Forms.Label label_resolution_original;
+        private System.Windows.Forms.Label label_resolution_scale;
+    }
+}
+
---
+++ Project/ScaleImageApp/ScaleApp_form.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form_main : Form
+    {
+        public Bitmap Image;      // исходное изображение
+        public Bitmap ScaleImage; // итоговое изображение
+
+        public Form_main() => InitializeComponent();
+
+        private void button_load_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Открытие изображения";
+                openFileDialog.InitialDirectory = "C:\\";
+                openFileDialog.Filter = "Image Files|*.jpg; *.jpeg; *.png; *.bmp; *.gif; *.ico";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        Image = new Bitmap(openFileDialog.FileName);
+                        if (Image.Width <= 1 && Image.Height <= 1)
+                        {
+                            MessageBox.Show("Изображение должно быть больше 1x1!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            return;
+                        }
+                        if (Image.Width > 720 && Image.Height > 480)
+                        {
+                            MessageBox.Show("Изображение должно быть не больше 720x480!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            return;
+                        }
+                        pictureBox_original_image.Image = Image;
+                        if (pictureBox_scale_image.Image != null)
+                        {
+                            pictureBox_scale_image.Image.Dispose();
+                            pictureBox_scale_image.Image = null;
+                        }
+                        if (label_resolution_scale.Visible) label_resolution_scale.Visible = false;
+                        label_resolution_original.Visible = true;
+                        label_resolution_original.Text = "Разрешение: " + Image.Width + " x " + Image.Height;
+                        button_scale.Enabled = true;
+                        button_save_image.Enabled = false;
+                        groupBox_settings.Enabled = true;
+                    }
+                    catch
+                    {
+                        DialogResult rezult = MessageBox.Show("Невозможно открыть выбранное изображение!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void button_scale_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox_scale.Text, out int number))
+            {
+                MessageBox.Show("В поле 'Коэффициент масштабирования' должно быть число >= 1!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox_scale.Focus();
+                textBox_scale.Clear();
+                return;
+            }
+            if (!(int.Parse(textBox_scale.Text) >= 1) || !(int.Parse(textBox_scale.Text) <= 50))
+            {
+                MessageBox.Show("В поле 'Коэффициент масштабирования' должно быть число >= 1 и <= 50!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox_scale.Focus();
+                textBox_scale.Clear();
+                return;
+            }
+            if (!radioButton_bilinear.Checked && !radioButton_bicubic.Checked)
+            {
+                MessageBox.Show("Не выбран алгоритм интерполяции!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            
+            int k = int.Parse(textBox_scale.Text); // коэффициент масштабирования
+            if (k == 1)
+            {
+                ScaleImage = new Bitmap(Image.Width * k, Image.Height * k);
+                ScaleImage = Image;
+                pictureBox_scale_image.Image = Image;
+                button_save_image.Enabled = true;
+                label_resolution_scale.Visible = true;
+                label_resolution_scale.Text = "Разрешение: " + ScaleImage.Width + " x " + ScaleImage.Height;
+                return;
+            }
+
+            ScaleImage = new Bitmap(Image.Width * k, Image.Height * k);
+            double[] X = new double[Image.Width];  // координаты X опорных точек в новом изображнии
+            double[] Y = new double[Image.Height]; // координаты Y опорных точек в новом изображнии
+            int D = 3;                             // размерность вектора    
+            double[] F = new double[Image.Width * Image.Height * D]; // массив с цветами каждой точки исходного изображения  
+
+            for (var i = 0; i < Image.Width; i++)
+                for (var j = 0; j < Image.Height; j++)
+                {
+                    X[i] = i * k + (k - 1.0) / 2.0;
+                    Y[j] = j * k + (k - 1.0) / 2.0;
+                    Color color = Image.GetPixel(i, j);
+                    F[D * (Image.Width * j + i)] =     color.R;
+                    F[D * (Image.Width * j + i) + 1] = color.G;
+                    F[D * (Image.Width * j + i) + 2] = color.B;
+                }
+
+            alglib.spline2dinterpolant spline;
+            if (radioButton_bilinear.Checked) alglib.spline2dbuildbilinearv(X, X.Length, Y, Y.Length, F, D, out spline);
+            else                              alglib.spline2dbuildbicubicv (X, X.Length, Y, Y.Length, F, D, out spline);
+
+            for (var i = 0; i < ScaleImage.Width; i++)
+                for (var j = 0; j < ScaleImage.Height; j++)
+                {
+                    // массив с цветами точки итогового изображения, хранит только 3 составляющие (R,G,B) текущей рассчитанной точки
+                    alglib.spline2dcalcv(spline, i, j, out double[] rgb);
+                    for (int l = 0; l < rgb.Length; l++)
+                    {
+                        if (rgb[l] < 0) rgb[l] = 0; if (rgb[l] > 255) rgb[l] = 255;
+                    }
+                    ScaleImage.SetPixel(i, j, Color.FromArgb((int)rgb[0], (int)rgb[1], (int)rgb[2]));
+                    pictureBox_scale_image.Image = ScaleImage;
+                }
+            button_save_image.Enabled = true;
+            label_resolution_scale.Visible = true;
+            label_resolution_scale.Text = "Разрешение: " + ScaleImage.Width + " x " + ScaleImage.Height;
+        }
+
+        private void button_save_image_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Title = "Сохранение изображения";
+                saveFileDialog.InitialDirectory = "C:\\";
+                saveFileDialog.Filter = "Jpg Image|*.jpg|Jpeg Image|*.jpeg|BitmapImage|*.bmp|Gif Image|*.gif|Portable Network Graphics Image|*.png|Icon Image|*.ico";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        ImageFormat format = ImageFormat.Bmp;
+                        string ext = System.IO.Path.GetExtension(saveFileDialog.FileName);
+                        switch (ext)
+                        {
+                            case ".jpg":
+                                format = ImageFormat.Jpeg;
+                                break;
+                            case ".jpeg":
+                                format = ImageFormat.Jpeg;
+                                break;
+                            case ".bmp":
+                                format = ImageFormat.Bmp;
+                                break;
+                            case ".gif":
+                                format = ImageFormat.Gif;
+                                break;
+                            case ".png":
+                                format = ImageFormat.Png;
+                                break;
+                            case ".ico":
+                                format = ImageFormat.Icon;
+                                break;
+                        }
+                        ScaleImage.Save(saveFileDialog.FileName, format);
+                        DialogResult rezult = MessageBox.Show("Изображение успешно сохранено!", "Оповещение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch
+                    {
+                        DialogResult rezult = MessageBox.Show("Невозможно сохранить изображение!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void Form_main_FormClosing(object sender, FormClosingEventArgs e) => e.Cancel = MessageBox.Show("Вы действительно хотите выйти из программы?", "Завершение программы", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.No;
+    }
+}
